aa5187a5715abaac995e1b9d70f439ae6b2b79d9
usuichuma <90407346+usuichuma@users.noreply.github.com>
2021-09-23 22:24:05 +0900

    Update human.rb

--- human.rb
+++ human.rb
@@ -4,7 +4,7 @@ require './thinkable'
 class Human < Animal
   include Thinkable
   
-  attr_accessor :name, :age, :hobby
+  attr_accessor :hobby
   
   def initialize(name, age ,hobby)
     self.name = name
